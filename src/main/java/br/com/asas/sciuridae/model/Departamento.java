0ec13eeaf8da66839e0fd7ea1536c9a53e759abb
Alex souza <aalex_sandro@hotmail.com>
2019-03-23 00:11:21 -0300

    configuração de model's para acessar o banco de dados.

--- src/main/java/br/com/asas/sciuridae/model/Departamento.java
+++ src/main/java/br/com/asas/sciuridae/model/Departamento.java
@@ -1,8 +1,17 @@
 package br.com.asas.sciuridae.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "departamento")
 public class Departamento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
     private Integer codigo;
+
+    @Column(name = "nome_departamento", nullable = false, unique = true)
     private String departamento;
 
     public Integer getCodigo() {
--- src/main/java/br/com/asas/sciuridae/model/Fabricante.java
+++ src/main/java/br/com/asas/sciuridae/model/Fabricante.java
@@ -1,8 +1,17 @@
 package br.com.asas.sciuridae.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fabricante")
 public class Fabricante {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cod_fabricante")
     private Integer codigo;
+
+    @Column(name = "nome_fabricante", nullable = false, unique = true)
     private String fabricante;
 
     public Integer getCodigo() {
